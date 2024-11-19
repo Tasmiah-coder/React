a5e243d1b87235912f3d2e90d045bea35c1562b9
Tasmiah-coder <tasmiahtabassum13218@gmail.com>
2024-11-19 20:23:26 +0600

    commit

---
+++ react/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Hello world</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, ipsam?</p>
+    </>
+  )
+}
+
+export default App
